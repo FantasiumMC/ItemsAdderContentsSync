34b14ff7a65fecae91906d8625a6f757cb5046cf
EpicPlayerA10 <adasko.lesiak@gmail.com>
2023-12-21 00:02:54 +0100

    Update modelengine to version 4.0.4

--- src/main/java/com/epicplayera10/itemsaddercontentssync/listeners/ModelEngineListener.java
+++ src/main/java/com/epicplayera10/itemsaddercontentssync/listeners/ModelEngineListener.java
@@ -2,13 +2,14 @@ package com.epicplayera10.itemsaddercontentssync.listeners;
 
 import com.epicplayera10.itemsaddercontentssync.ItemsAdderContentsSync;
 import com.ticxo.modelengine.api.events.ModelRegistrationEvent;
+import com.ticxo.modelengine.api.generator.ModelGenerator;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class ModelEngineListener implements Listener {
     @EventHandler
     public void onFinalPhase(ModelRegistrationEvent event) {
-        if (event.getPhase() == ModelRegistrationEvent.Phase.FINAL) {
+        if (event.getPhase() == ModelGenerator.Phase.FINISHED) {
             ItemsAdderContentsSync.instance().getThirdPartyPluginStates().modelEngineReloadingFuture.complete(null);
         }
     }
